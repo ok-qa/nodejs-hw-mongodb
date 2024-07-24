33ec3eba5730c8d6369a6be272dd9f23e5ea5463
ok-qa <olechka.kr@gmail.com>
2024-07-24 13:20:40 -0700

    Update initMongoConnection.js

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -13,7 +13,7 @@ export const initMongoConnection = async () => {
     );
     console.log('Mongo connection successfully established!');
   } catch (e) {
-    console.error('Error while setting up mongo connection', e);
+    console.log('Error while setting up mongo connection', e);
     throw e;
   }
 };
