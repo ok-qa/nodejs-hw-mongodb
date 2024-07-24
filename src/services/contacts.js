c48bbb704087746301a75935e4cf838bf8c8142b
ok-qa <olechka.kr@gmail.com>
2024-07-24 12:34:09 -0700

    import updated

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,4 +1,4 @@
-import { ContactsCollection } from '../db/models/contact';
+import { ContactsCollection } from '../db/models/contact.js';
 
 export const getAllContacts = async () => {
   const contacts = await ContactsCollection.find();
