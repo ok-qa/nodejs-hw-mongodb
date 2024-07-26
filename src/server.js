c1a7ab44a1637ca94fb6fd74e319533d6366d055
ok-qa <olechka.kr@gmail.com>
2024-07-26 10:02:09 -0700

    updated server.js

--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ const PORT = Number(env('PORT', '3000'));
 
 const setupServer = () => {
   const app = express();
+  app.use(express.json());
   app.use(cors());
   app.use(
     pino({
@@ -19,11 +20,6 @@ const setupServer = () => {
       },
     }),
   );
-  app.use('/*', (req, res, next) => {
-    res.status(404).json({
-      message: 'Not found',
-    });
-  });
 
   app.get('/contacts', async (req, res) => {
     const contacts = await getAllContacts();
@@ -45,6 +41,12 @@ const setupServer = () => {
     res.status(200).json({
       data: contact,
       message: `Successfully found contact with id ${contactId}`,
+    });
+  });
+
+  app.use('*', (req, res, next) => {
+    res.status(404).json({
+      message: 'Not found',
     });
   });
 
