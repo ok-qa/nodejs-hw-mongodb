ffbd82bef1d2afcc43910b52f68ee7e549cc27ab
ok-qa <olechka.kr@gmail.com>
2024-07-28 16:43:39 -0700

    deleted extra folder middleware from controllers folder

--- src/controllers/middlewares/errorHandler.js
+++
@@ -1,6 +0,0 @@
-export const errorHandler = (err, req, res, next) => {
-  res.status(500).json({
-    message: 'Something went wrong',
-    error: err.message,
-  });
-};
--- src/controllers/middlewares/notFoundHandler.js
+++
@@ -1,5 +0,0 @@
-import createHttpError from 'http-errors';
-
-export const notFoundHandler = (req, res, next) => {
-  return next(createHttpError(404, 'Route not found'));
-};
