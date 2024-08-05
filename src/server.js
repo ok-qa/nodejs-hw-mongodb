ed2831fac00d82db9cb51a7a763ac42a1eee8971
ok-qa <olechka.kr@gmail.com>
2024-08-05 01:15:47 -0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -29,12 +29,6 @@ const setupServer = () => {
 
   app.use(errorHandler);
 
-  // app.use('*', (req, res, next) => {
-  //   res.status(404).json({
-  //     message: 'Not found',
-  //   });
-  // });
-
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
