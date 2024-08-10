030c583f274cac9433ad0e1a75d43c833f2c259c
ok-qa <olechka.kr@gmail.com>
2024-08-09 19:10:15 -0700

    updated validation schema for PATCH

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -8,7 +8,10 @@ import {
 } from '../controllers/contacts.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import { validateBody } from '../middlewares/validateBody.js';
-import { createContactSchema } from '../validation/contacts.js';
+import {
+  createContactSchema,
+  updateContactSchema,
+} from '../validation/contacts.js';
 import { isValidId } from '../middlewares/isValidId.js';
 
 const router = Router();
@@ -26,7 +29,7 @@ router.post(
 router.patch(
   '/:contactId',
   isValidId,
-  validateBody(createContactSchema),
+  validateBody(updateContactSchema),
   ctrlWrapper(patchContactController),
 );
 
--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -10,3 +10,11 @@ export const createContactSchema = Joi.object({
     .valid('work', 'home', 'personal')
     .default('personal'),
 });
+
+export const updateContactSchema = Joi.object({
+  name: Joi.string().min(3).max(20),
+  phoneNumber: Joi.string().min(3).max(20),
+  email: Joi.string().email(),
+  isFavourite: Joi.boolean(),
+  contactType: Joi.string().valid('work', 'home', 'personal'),
+}).or('name', 'phone', 'email', 'isFavourite', 'contactType');
