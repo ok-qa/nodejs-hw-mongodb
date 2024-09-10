41c0c54261b366c8df57cf17bd052cf1880f3d58
ok-qa <olechka.kr@gmail.com>
2024-09-10 01:19:03 -0700

    updated auth.js

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -7,8 +7,8 @@ import {
   requestResetToken,
   resetPassword,
 } from '../services/auth.js';
-import { generateAuthUrl } from '../utils/googleOAuth2.js';
-import { loginOrSignupWithGoogle } from '../services/auth.js';
+// import { generateAuthUrl } from '../utils/googleOAuth2.js';
+// import { loginOrSignupWithGoogle } from '../services/auth.js';
 
 export const registerUserController = async (req, res) => {
   const user = await registerUser(req.body);
